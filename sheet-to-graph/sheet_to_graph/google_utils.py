1cb4663a81c1710eb06b69a43f747b3952ddd215
George <george.alexander.wright@gmail.com>
2025-11-13 11:01:11 +0000

    use user to authenticate instead of service

--- sheet-to-graph/sheet_to_graph/google_utils.py
+++ sheet-to-graph/sheet_to_graph/google_utils.py
@@ -1,32 +1,53 @@
 import os
-
-from google.oauth2 import service_account
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 
 
 class GoogleUtils:
+    """
+    Utilities for building Google API service clients using a user's OAuth credentials.
+    """
+
     _drive_service = None
     _sheets_service = None
+
     _SCOPES = [
-        "https://www.googleapis.com/auth/spreadsheets.readonly",
         "https://www.googleapis.com/auth/drive",
+        "https://www.googleapis.com/auth/spreadsheets",
     ]
 
     @classmethod
     def _creds(cls):
-        creds_file_path = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
-        return service_account.Credentials.from_service_account_file(
-            creds_file_path, scopes=cls._SCOPES
+        """
+        Build Credentials from the OAuth client ID / secret / refresh token
+        stored in environment variables (or Secret Manager).
+        """
+        client_id = os.environ["OAUTH_CLIENT_ID"]
+        client_secret = os.environ["OAUTH_CLIENT_SECRET"]
+        refresh_token = os.environ["OAUTH_REFRESH_TOKEN"]
+
+        creds = Credentials(
+            token=None,
+            refresh_token=refresh_token,
+            token_uri="https://oauth2.googleapis.com/token",
+            client_id=client_id,
+            client_secret=client_secret,
+            scopes=cls._SCOPES,
         )
+        creds.refresh(Request())
+        return creds
 
     @classmethod
     def get_drive_service(cls):
+        """Return a cached Google Drive API service."""
         if cls._drive_service is None:
             cls._drive_service = build("drive", "v3", credentials=cls._creds())
         return cls._drive_service
 
     @classmethod
     def get_sheets_service(cls):
+        """Return a cached Google Sheets API service."""
         if cls._sheets_service is None:
             cls._sheets_service = build("sheets", "v4", credentials=cls._creds())
         return cls._sheets_service
